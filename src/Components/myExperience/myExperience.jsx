add0358919fe9893dc66e87bc8455cdc08780e75
Rohit Devhare <rohitdevhare400@gmail.com>
2024-07-20 20:39:39 +0530

    added mobile responsive changes

---
+++ src/Components/myExperience/myExperience.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './myExperience.css'
+
+const MyExperience = () => {
+
+    return (
+        <div className="about-container">
+            <div>
+                <h1 className='about-heading'> About ME </h1>
+                <p className='coming-soon-text'> COMING SOON ......</p>
+            </div>
+        </div>
+    );
+};
+
+export default MyExperience;
